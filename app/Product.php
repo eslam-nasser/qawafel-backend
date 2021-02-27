9b22cdb03412bf3ec693315e26015ca5ddea8cc8
eslam-nasser <in1146@fayoum.edu.eg>
2021-02-27 16:20:16 +0200

    feat(categories-products-relationship) : somehow I got a many-to-many relationship to work on the first try!

--- app/Product.php
+++ app/Product.php
@@ -18,4 +18,9 @@ class Product extends Model
     {
         return $this->belongsTo(Vendor::class);
     }
+
+    public function categories()
+    {
+        return $this->belongsToMany(Category::class);
+    }
 }
