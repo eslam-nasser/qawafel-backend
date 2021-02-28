db7942ff31982a59b08f5a35ea19a85ddde8761b
eslam-nasser <in1146@fayoum.edu.eg>
2021-02-28 10:43:56 +0200

    feat(cart) : init cart resource

---
+++ database/migrations/2021_02_28_082437_create_carts_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCartsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('carts', function (Blueprint $table) {
+            $table->id();
+            $table->foreign('user_id')->nullable()->references('id')->on('users');
+            $table->string('mobile');
+            $table->string('email');
+            $table->string('address1');
+            $table->string('address2');
+            $table->string('city');
+            $table->string('province');
+            $table->string('country');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('carts');
+    }
+}
