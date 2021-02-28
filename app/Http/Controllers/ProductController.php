f766176df18fbb2b63db25d6a58373fe069bad11
eslam-nasser <in1146@fayoum.edu.eg>
2021-02-28 11:48:21 +0200

    refactor(products) : populate vendor info

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -15,7 +15,7 @@ class ProductController extends Controller
      */
     public function index()
     {
-        return Product::with('categories')->get();
+        return Product::with(['categories', 'vendor'])->get();
     }
 
 
